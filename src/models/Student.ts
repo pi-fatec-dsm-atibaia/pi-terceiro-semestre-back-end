51d5a39a30bc544710b9b6915fd68ac6b66f81c1
Gusta Gevigi <gustavocesargevigi@gmail.com>
2025-11-01 13:28:02 -0300

    feat: atualiza model e controller de Student com novos atributos

--- src/models/Student.ts
+++ src/models/Student.ts
@@ -3,68 +3,42 @@ import sequelize from '../config/database';
 
 export interface StudentAttributes {
   id?: number;
-  nome: string;
-  cpf: string;
+  idCurso: number;
   ra: string;
   rg: string;
-  senha: string;
-  email: string;
   telefone: string;
+  nome: string;
+  email: string;
+  cpf: string;
+  senha: string;
 }
 
 class Student extends Model<StudentAttributes> implements StudentAttributes {
   public id!: number;
-  public nome!: string;
-  public cpf!: string;
+  public idCurso!: number;
   public ra!: string;
   public rg!: string;
-  public senha!: string;
-  public email!: string;
   public telefone!: string;
+  public nome!: string;
+  public email!: string;
+  public cpf!: string;
+  public senha!: string;
 }
 
 Student.init({
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true
-  },
-  nome: {
-    type: DataTypes.STRING(100),
-    allowNull: false
-  },
-  cpf: {
-    type: DataTypes.STRING(11),
-    allowNull: false,
-    unique: true
-  },
-  ra: {
-    type: DataTypes.STRING(20),
-    allowNull: false,
-    unique: true
-  },
-  rg: {
-    type: DataTypes.STRING(15),
-    allowNull: false,
-    unique: true
-  },
-  senha: {
-    type: DataTypes.STRING(255),
-    allowNull: false
-  },
-  email: {
-    type: DataTypes.STRING(100),
-    allowNull: false,
-    unique: true
-  },
-  telefone: {
-    type: DataTypes.STRING(15),
-    allowNull: false
-  }
+  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+  idCurso: { type: DataTypes.INTEGER },
+  ra: { type: DataTypes.STRING(20), allowNull: false },
+  rg: { type: DataTypes.STRING(20), allowNull: false },
+  telefone: { type: DataTypes.STRING(20), allowNull: false },
+  nome: { type: DataTypes.STRING(100), allowNull: false },
+  email: { type: DataTypes.STRING(100), allowNull: false },
+  cpf: { type: DataTypes.STRING(14), allowNull: false },
+  senha: { type: DataTypes.STRING(100), allowNull: false }
 }, {
   sequelize,
-  modelName: 'Student',
-  tableName: 'students'
+  tableName: 'Aluno',
+  modelName: 'Student'
 });
 
 export default Student;
